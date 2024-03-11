cc4902a77adae252033f8586d1e2ca217b57584f
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-03-11 17:01:21 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "atyra-my-ecs-bucket"
+    bucket = "atyra-eks-bucket"
     key    = "tf-state/eks"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
